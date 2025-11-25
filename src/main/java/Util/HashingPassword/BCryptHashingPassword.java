216909bad396e6f31df5636889213cbc3fda9a76
Baraa Abuhalima <B.K.1Baraa@gmail.com>
2025-11-25 21:56:29 +0200

    SDFF

--- src/main/java/Util/HashingPassword/BCryptHashingPassword.java
+++ src/main/java/Util/HashingPassword/BCryptHashingPassword.java
@@ -3,6 +3,7 @@ package Util.HashingPassword;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class BCryptHashingPassword implements IHashingPassword {
+    int bara;
     @Override
     public String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt(12));
