c59e475e59645fbe91422205a651c59d4a57a0cc
Baraa Abuhalima <B.K.1Baraa@gmail.com>
2025-11-25 21:47:21 +0200

    fix checkstyle bug

--- src/main/java/Util/HashingPassword/IHashingPassword.java
+++ src/main/java/Util/HashingPassword/IHashingPassword.java
@@ -1,6 +1,8 @@
 package Util.HashingPassword;
 
 public interface IHashingPassword {
+
     public String hashPassword(String password);
+
     public boolean verifyPassword(String password, String hashedPassword);
 }
